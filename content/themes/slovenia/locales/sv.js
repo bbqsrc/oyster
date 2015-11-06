bcf63a98b4243709077004aa710a3fa1af2e98b4
Brendan Molloy <brendan@bbqsrc.net>
2015-11-06 15:23:44 +1100

    Add secret shame

---
+++ content/themes/slovenia/locales/sv.js
@@ -0,0 +1,10 @@
+{
+  "How do you wish to vote on this motion?": "Hur vill du att rösta om detta förslag?",
+  "Aye": "Ja",
+  "Abstain": "Avstå",
+  "Nay": "Nej",
+  "This application requires JavaScript to be enabled to function. Please enable JavaScript and reload the page.": "Den här applikationen kräver att JavaScript är aktiverat för att fungera. Vänligen aktivera JavaScript och ladda om sidan.",
+  "Please ensure that you have completed your ballot. Once you submit, you cannot go back.": "Se till att du har avslutat din sluten omröstning. När du har skickat, kan du inte gå tillbaka.",
+  "Submit": "Lämna in",
+  "Powered by %s": "Drivas av %s"
+}
