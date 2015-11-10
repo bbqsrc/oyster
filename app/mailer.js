530f2b136a933217c84c8b1935a1b52c3e912cdf
Brendan Molloy <brendan@bbqsrc.net>
2015-11-10 23:59:09 +1100

    License autogeneration fail

--- app/mailer.js
+++ app/mailer.js
@@ -14,7 +14,6 @@
  *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- * <one line to give the program's name and a brief idea of what it does.>
  */
 'use strict'; /* eslint-disable no-unused-vars */
 
